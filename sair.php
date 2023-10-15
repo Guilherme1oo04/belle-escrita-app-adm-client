c28effa8860d50249a508ef28d53e07378b88c88
Guilherme Chaves <guilherme.9999.chaves@gmail.com>
2023-10-15 12:07:37 -0300

    corrigindo falha de segurança - conexão mysqli estava ficando aberta

--- sair.php
+++ sair.php
@@ -1,8 +1,12 @@
 <?php
+    include("conexao.php");
+
     session_start();
 
     unset ($_SESSION['nomeadm']);
     unset ($_SESSION['senhaadm']);
+
+    mysqli_close($conn);
 
     header("location: index.php");
 ?>
