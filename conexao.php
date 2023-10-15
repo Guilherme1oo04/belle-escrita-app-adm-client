8e24efdc40e62509ab44a99cb8fcd66a8537f656
Guilherme Chaves <guilherme.9999.chaves@gmail.com>
2023-10-15 12:05:36 -0300

    acessando bd atraves do .env

--- conexao.php
+++ conexao.php
@@ -1,9 +1,15 @@
 <?php
-$servidor = "localhost";
-$usuario = "root";
-$senha = "";
-$dbname = "belleescrita";
 
-//Criar a conexao
-$conn = mysqli_connect($servidor, $usuario, $senha, $dbname);
+    require_once realpath(__DIR__ . '/vendor/autoload.php');
+
+    $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+    $dotenv->load();
+
+    $servidor = $_SERVER["DB_HOST"];
+    $usuario = $_SERVER["DB_USER"];
+    $senha = $_SERVER["DB_PASSWORD"];
+    $dbname = $_SERVER["DB_DATABASE"];
+
+    //Criar a conexao
+    $conn = mysqli_connect($servidor, $usuario, $senha, $dbname);
 ?>
